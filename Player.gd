7785bf1a5bb85114666dee250c6fb13726350d25
Trystan Hendricks <dolkhound@gmx.com>
2020-02-24 20:47:38 -0500

    got the 4 way walking working

--- Player.gd
+++ Player.gd
@@ -1,30 +1,24 @@
 extends KinematicBody2D
 
 export (int) var speed = 200
+const MAX_SPEED = 400
 
 var velocity = Vector2()
 
-func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed('ui_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('ui_down'):
-		velocity.y += 1
-	if Input.is_action_pressed('ui_up'):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
 
-func _physics_process(delta):
-	get_input()
-	velocity = move_and_slide(velocity)
-	
 func _process(delta):
 	if Input.is_action_pressed('ui_right'):
 		$AnimatedSprite.play('walk_right')
+		position.x += MAX_SPEED * delta
 	elif Input.is_action_pressed('ui_left'):
 		$AnimatedSprite.play('walk_left')
+		position.x -= MAX_SPEED * delta
+	elif Input.is_action_pressed('ui_up'):
+		$AnimatedSprite.play('walk_up')
+		position.y -= MAX_SPEED * delta
+	elif Input.is_action_pressed('ui_down'):
+		$AnimatedSprite.play('walk_down')
+		position.y += MAX_SPEED * delta
 	else:
 		$AnimatedSprite.stop()
 
