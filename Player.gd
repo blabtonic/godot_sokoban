c60e0178f2ed38f4f9bcfb5dbddca3cea7f1b6d7
Trystan Hendricks <dolkhound@gmx.com>
2020-02-24 21:24:58 -0500

    added collision to player

--- Player.gd
+++ Player.gd
@@ -1,12 +1,16 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 100
 const MAX_SPEED = 400
 
 var velocity = Vector2()
 
 
-func _process(delta):
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.slide(collision.normal)
+	
 	if Input.is_action_pressed('ui_right'):
 		$AnimatedSprite.play('walk_right')
 		position.x += MAX_SPEED * delta
